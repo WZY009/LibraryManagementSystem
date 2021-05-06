548cb58abc5985d65440bbf6246e91f1fde0aeb3
Ed Song <sunrainshr@qq.com>
2021-05-06 13:12:10 +0800

    添加Migations

---
+++ BookMS/Migrations/20210506051048_InitialDatabase.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace BookMS.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(767)", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(767)", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Author = table.Column<string>(type: "text", nullable: true),
+                    Press = table.Column<string>(type: "text", nullable: true),
+                    Number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(767)", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Sex = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lends",
+                columns: table => new
+                {
+                    No = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Uid = table.Column<string>(type: "varchar(767)", nullable: true),
+                    Bid = table.Column<string>(type: "varchar(767)", nullable: true),
+                    LendTime = table.Column<DateTime>(type: "datetime", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lends", x => x.No);
+                    table.ForeignKey(
+                        name: "FK_Lends_Books_Bid",
+                        column: x => x.Bid,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Lends_Users_Uid",
+                        column: x => x.Uid,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Admins",
+                columns: new[] { "Id", "Password" },
+                values: new object[] { "admin", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Name", "Number", "Press" },
+                values: new object[,]
+                {
+                    { "978-7-302-34695-1", "尹为民", "数据库原理与技术", 20, "清华大学出版社" },
+                    { "978-7-111-50482-5", "David A. Patterson", "计算机组成与设计", 10, "机械工业出版社" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name", "Password", "Sex" },
+                values: new object[,]
+                {
+                    { "wzy", "万兆奕", "wzy", true },
+                    { "shr", "宋昊睿", "shr", true }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lends_Bid",
+                table: "Lends",
+                column: "Bid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lends_Uid",
+                table: "Lends",
+                column: "Uid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Lends");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
