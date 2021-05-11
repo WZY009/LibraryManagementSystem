f73f24c69610cc0f5b4c62312c5acea35b4d7184
Ed Song <sunrainshr@qq.com>
2021-05-11 23:38:40 +0800

    更换库

--- BookMS/Program.cs
+++ BookMS/Program.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BookMS
-{
-    static class Program
-    {
+namespace BookMS {
+    static class Program {
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             System.Windows.Forms.Application.Run(new adminNewManagecs());//表示从login开始运行
