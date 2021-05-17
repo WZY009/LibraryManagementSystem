8af7cec6a291ed2ce47c085823cbb6927bcf5cdc
Ed Song <sunrainshr@qq.com>
2021-05-17 16:05:07 +0800

    删除迁移记录

--- BookMS/Models/User.cs
+++ BookMS/Models/User.cs
@@ -10,10 +10,19 @@ namespace BookMS.Models {
 
         [Key]
         public string Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public bool Sex { get; set; }
+        [Required]
+        // 在发行为二进制文件时添加密码的最小长度限制，在数据库迁移时反注释下一行
+        //[MinLength(6)]
+        [MaxLength(30)]
         public string Password { get; set; }
         public string PhotoPath { get; set; }
         public string Major { get; set; }
+        [Required]
+        public int VerifyQuestion { get; set; }
+        [Required]
+        public string VerifyAnswer { get; set; }
     }
 }
